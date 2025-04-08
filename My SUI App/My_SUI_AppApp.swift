2ec9ff39f99d81a416286d1e5087321a88366dfc
AlexCoder13 <sesasha99@gmail.com>
2025-04-08 16:18:07 +0300

    Initial Commit

---
+++ My SUI App/My_SUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_SUI_AppApp.swift
+//  My SUI App
+//
+//  Created by Александр Семёнов on 08.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct My_SUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
