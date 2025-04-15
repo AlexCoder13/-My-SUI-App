1dd1cea232b1d33983ef213ad5fce4351736eb4f
AlexCoder13 <sesasha99@gmail.com>
2025-04-16 00:11:08 +0300

    SUI app

--- My SUI App/App/MyApp.swift
+++ My SUI App/App/MyApp.swift
@@ -4,14 +4,16 @@
 //
 //  Created by Александр Семёнов on 08.04.2025.
 //
-
 import SwiftUI
 
 @main
-struct My_SUI_AppApp: App {
+struct MyApp: App {
     var body: some Scene {
         WindowGroup {
+            
             ContentView()
+            
         }
     }
+    
 }
