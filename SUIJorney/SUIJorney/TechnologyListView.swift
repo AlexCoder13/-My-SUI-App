e3c17a857ee24324ffe29e3ab842b5c90ee7f334
AlexCoder13 <sesasha99@gmail.com>
2025-07-09 00:01:06 +0300

    sui: navigation

--- SUIJorney/SUIJorney/TechnologyListView.swift
+++ SUIJorney/SUIJorney/TechnologyListView.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 
+struct TechnologyDetailView: View {
+    var technology: Technology
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: technology.imageName)
+                .font(.system(size: 80))
+                .foregroundStyle(.orange)
+            Text(technology.name)
+                .font(.title)
+        }
+        .navigationTitle(technology.name)
+    }
+}
+
 struct Technology: Identifiable {
     let id = UUID()
     let name: String
@@ -23,17 +38,23 @@ let technologies = [
 struct TechnologyListView: View {
     var body: some View {
         
-        List(technologies) { technology in
-            HStack{
-                Image(systemName: technology.imageName)
-                    .font(.title) // Для SF Symbols лучше использовать .font, а не .resizable
-                    .frame(width: 35) // Фиксированная ширина для выравнивани
-                Text(technology.name)
-                
-                Spacer()
+        NavigationStack {
+            List(technologies) { technology in
+                NavigationLink {
+                    TechnologyDetailView(technology: technology)
+                } label: {
+                    HStack {
+                        Image(systemName: technology.imageName)
+                            .font(.title) // Для SF Symbols лучше использовать .font, а не .resizable
+                            .frame(width: 35) // Фиксированная ширина для выравнивани
+                        Text(technology.name)
+                        
+                        Spacer()
+                    }
+                }
             }
+            .navigationTitle("Технологии Apple")
         }
-        
     }
 }
 
